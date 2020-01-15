98773bd756c2c0c6df7133c80a7d9819ee4b634b
aswanthkoleri <aswanth9495@gmail.com>
2020-01-15 16:02:31 +0530

    Rewrite the codes using redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-import React, { Component } from 'react';
-import { Route, BrowserRouter } from 'react-router-dom'
-import Home from './components/Home'
-import Create from './components/Create'
-import Navbar from './components/Navbar'
-import List from './components/List'
-import Show from './components/Show'
-import Edit from './components/Edit'
-import {connect} from 'react-redux' 
-import { getInterviews } from './actions/getInterviews'
+import React, { Component } from "react";
+import { Route, BrowserRouter } from "react-router-dom";
+import Home from "./components/Home";
+import Create from "./components/Create";
+import Navbar from "./components/Navbar";
+import List from "./components/List";
+import Show from "./components/Show";
+import Edit from "./components/Edit";
+import { connect } from "react-redux";
+import { getInterviews } from "./actions/getInterviews";
 
 class App extends Component {
+  componentDidMount = () => {
+    this.props.dispatch(getInterviews());
+  };
 
-componentWillMount=()=>{
-        this.props.dispatch(getInterviews())
-}
-
-render(){
-  return (
-    <BrowserRouter>
+  render() {
+    return (
+      <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Route exact path='/' component={Home}/>
-          <Route path='/create' component={Create} />
-          <Route path='/list' component={List} />
-          <Route path ='/show/:id' component={Show} />
-          <Route path ='/edit/:id' component={Edit} />
+          <Route exact path="/" component={Home} />
+          <Route path="/create" component={Create} />
+          <Route path="/list" component={List} />
+          <Route path="/show/:id" component={Show} />
+          <Route path="/edit/:id" component={Edit} />
         </div>
       </BrowserRouter>
-  );
+    );
   }
 }
-const mapStateToProps=(state) => {
-  return {
-  }
-}
+const mapStateToProps = state => {
+  return {};
+};
 export default connect(mapStateToProps)(App);
